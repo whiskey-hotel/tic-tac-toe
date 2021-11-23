b2314adb02e8ad5e8e9357268f28e9fbd3f85052
Justin Thornton <jathom15@gmail.com>
2021-11-22 23:15:31 -0600

    Player factory function

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const players = (name, playerType) => {
+	const _type = playerType;
+	const _playerPositions = [null, null, null, null, null, null, null, null, null];
+	const getName = () => name;
+
+	const updateChoices = (index) => {
+		_playerPositions[index] = _type;
+	};
+
+	return { getName, updateChoices };
+};
+
+const player1 = players("justin", "X");
+const player2 = players("justin's opponent", "O");
